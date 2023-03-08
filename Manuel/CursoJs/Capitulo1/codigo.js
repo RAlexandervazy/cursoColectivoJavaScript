055327ece2c7aa8339e46bfecb2244557ec24ca0
Manuelhaas99 <83323798+Manuelhaas99@users.noreply.github.com>
2023-03-07 22:57:33 -0600

    Manuel (#7)
    * Estoy agregando el Capitulo 1
    
    * Arregle la extension del archivo
    
    * Delete Capitulo1.txt
    
    * I add the fucking js script
    
    * Ya lo cambie alv
    
    * Agregue La Carpeta de Capitulo 1
    
    * Avance hasta prompt

---
+++ Manuel/CursoJs/Capitulo1/codigo.js
@@ -0,0 +1,29 @@
+//var Number = 15;
+//const nombre = "Dalto"
+//nombre = "suarez"\
+
+//let numero;
+
+//alert (numero);
+
+//numero =3;
+
+//let numero, numero2, numero3;
+
+//numero = 2;
+//numero2 = 9;
+//numero3 = 13;
+
+//alert(numero)
+
+//recipiente = "barbijo";
+
+//alert(recipiente)
+
+//let numero = null;
+
+let numero  = 5;
+let numero2 = "pedro";
+
+alert(numero * numero2);
+
